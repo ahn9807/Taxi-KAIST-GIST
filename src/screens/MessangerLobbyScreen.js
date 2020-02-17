d3bb1e8dd1dcadb7e4acad7a4f6a093e9093e788
Kyung Wook Nam <ruddnrdl@kaist.ac.kr>
2020-02-17 14:39:59 +0900

    .

--- src/screens/MessangerLobbyScreen.js
+++ src/screens/MessangerLobbyScreen.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { StyleSheet, View, Text } from "react-native";
 
 export default class MessangerLobbyScreen extends Component {
+
+
+    
     render() {
         return(
             <View style={styles.container}>
