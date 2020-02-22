a08d3e62f1960ebb54d9b24b007c80d6192da06d
Kyung Wook Nam <ruddnrdl@kaist.ac.kr>
2020-02-23 02:30:16 +0900

    2/23 wook pull 전

--- src/screens/ChatRoomScreen.js
+++ src/screens/ChatRoomScreen.js
@@ -21,7 +21,7 @@ export default class ChatRoomScreen extends Component {
 
   componentDidMount() {
     console.log("did")
-    Fire.shared.on(message => {
+    this.on(message => {
       this.setState(previousState => ({
         messages: GiftedChat.append(previousState.messages, message)
       }))
@@ -31,12 +31,10 @@ export default class ChatRoomScreen extends Component {
     console.log("why?")
     console.log(navigation)
 
-
-
   }
 
   componentWillMount() {
-    Fire.off();
+    this.off();
   }
 
   get user() {
@@ -46,20 +44,71 @@ export default class ChatRoomScreen extends Component {
       .routes
       .find(v => v.name === 'ChatRoom')
       .params
-      .name;
+      .username;
 
     return {
-      name: username,
-      _id: Fire.uid
+      username: username,
+      _id: this.uid()
     }
   }
+
+  refm() {
+    const roomname = this.props.navigation
+      .dangerouslyGetState()
+      .routes
+      .find(v => v.name === 'ChatRoom')
+      .params
+      .roomname;
+
+
+    return firebase.firestore().collection('ChatRooms')
+      .doc(roomname).collection('messages');
+    //roomname 확인해주어야 함.
+  }
+
+  uid() {
+    return (firebase.auth().currentUser || {}).uid;
+  }
+
+  off() {
+    this.refm().onSnapshot(function () {
+      console.log("off")
+    });
+  }
+
+  on = callback => this.refm().orderBy('createdAt', 'asc').onSnapshot(snapshot => {
+    snapshot.docChanges().forEach(change => {
+      if (change.type === 'added') {
+        callback(this.parse(change.doc))
+      }
+    })
+  });
+
+  parse = message => {
+    const { createdAt, text, user } = message.data();
+    const { id: _id } = message; //무슨 의미지...
+
+    return { _id, createdAt: Date(createdAt), text, user }
+  }
+
+  send = messages => {
+    for (let i = 0; i < messages.length; i++) {
+      const { text, user } = messages[i];
+      const message = { text, user, createdAt: new Date() };
+      this.append(message)
+    }
+  };
+
+
+  append = message => this.refm().add(message)
+
 
   render() {
     return (
       <View style={styles.chat}>
         <GiftedChat
           messages={this.state.messages}
-          onSend={Fire.shared.send}
+          onSend={this.send}
           user={this.user}
         ></GiftedChat>
         <KeyboardAvoidingView behavior='padding' keyboardVerticalOffset={80} />
--- src/screens/MessengerLobbyScreen.js
+++ src/screens/MessengerLobbyScreen.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Text } from "react-native";
-import { Button } from "react-native-elements";
+import { Button, ListItem } from "react-native-elements";
+import { TextInput } from "react-native-gesture-handler";
 import firebase from 'firebase'
+import 'firebase/firestore'
 // import {ChatRoom} from './ChatRoomScreen'
 // const navigator = createStackNavigator({
     
@@ -16,36 +18,115 @@ export default class MessengerLobbyScreen extends Component {
                 .get()
                 .then(function(user){
                     // console.log(user.data().displayName)
-                    this.setState({name: user.data().displayName})  
+                    this.setState({username: user.data().displayName})  
                     console.log("test")
                     // console.log(this.state.name)
                   
                     
                 }.bind(this)
             );
-
     }
 
+
     state = {
-        name: ''
+        username: '',
+        roomname:'room',
+        makename: '',
+        chatList:[]
     };
+
+    componentDidMount(){ //여기에 해주는게 맞나?
+        let chatList=[]
+        firebase.firestore()
+                .collection('ChatRooms')
+                .get()
+                .then(function(doc){
+                    console.log("ctrms")
+                    roomNames= doc
+                    roomNames.forEach(doc =>{
+                        json=doc.data()
+                        console.log(json)
+                        // 방 이름을 빼서 저장. reservationdata와 같이 관리 될거니 일단 삭제는 x.
+                        if(json.member.includes(firebase.auth().currentUser.uid)){
+                            console.log(json.roomname)
+                            chatList.push(json.roomname)
+                        }
+                    })
+                    this.setState({chatList: chatList})
+                })
+    }
 
     GoChat = () =>{
         console.log("gochat")
-        console.log(this.state.name)
+        // console.log(this.state.name)
         const { navigation } = this.props
         
-        navigation.navigate('ChatRoom', {name: this.state.name})     
-    } 
+        navigation.navigate('ChatRoom', {username: this.state.username,
+                                        roomname: this.state.roomname}) 
+                                        
+    }
+    
+    //채팅방을 만들어보아요
+    // 방 이름, 멤버에 대한 정보를 넣었는데, 후에 reservation screen에서 이를 해줄 것
+    // 이를 채팅방 데이터와 어떻게 합칠지 잘 생각해보아야 함.
+    MakeChat = () =>{
+        const { makeName } = this.state
+        let member=[ (firebase.auth().currentUser || {}).uid ]
+        const data={
+                roomname: makeName,
+                member: member
+        }
+
+        firebase.firestore().collection('ChatRooms')
+                            .doc(makeName)
+                            .set(data)
+
+        // firebase.firestore().doc('UserRooms')
+        //                     .collection(member)
+        //                     .doc('rooms')
+        //                     .get()
+        //                     .then(function(doc){
+
+        //                     })
+                    
+    }
+
+    
+
+
+
+
 
     render() {
         return(
             <View style={styles.container}>
-                <Text>
+                {
+                    this.state.chatList.map((l, i) =>(
+                        <ListItem
+                            key={i}
+                            title={l.name}
+                            subtitle="chatroom init version"
+                            bottomDivider
+                            />
+                    ))
+                }
+                <Text >
                     Messenger Lobby Screen
                 </Text>
-                <Button title="Go Chat" onPress={this.GoChat}> 
+                <Button title="Go Chat" onPress={this.GoChat} style={{ marginTop: 30 }} >
                 </Button>
+                <Text style={{ marginTop: 30 }}>
+                    ChatRoom Name
+                </Text>
+                <TextInput 
+                style={{ marginTop: 30 }}
+                    placeholder='chat name'
+                    onChangeText={text => this.setState({ makeName: text })}
+                    value={this.state.makeName}
+                    style={{ marginTop: 30 }}
+                />
+
+                <Button title="Make Chat " onPress={this.MakeChat } style={{ marginTop: 30 }}> </Button>
                 
             </View>
         )
