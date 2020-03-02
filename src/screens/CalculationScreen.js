7cb9020094b1ac7a517435242f2ba8a93c721d9b
Kyung Wook Nam <ruddnrdl@kaist.ac.kr>
2020-03-03 06:09:27 +0900

    3/2 wook 정산 기능 초안 - Date를 이용해 정산 중, 탑승 예정인 택시팟 분류, reservationdata에서의 삭제는 정산 완료시에 할 생각 - 정산 시작 slide panel 제작. - calculation collection 관한 network code 작성 - 정산 팟들을 calculation 여부로 버튼 분류 - 정산 시 host field를 만들고, accordionlist를 이용해 받을 돈, 보낼 돈 목록 분류,
    - 안의 버튼 누르면 history db로 옮기고 정산 종료, reservation에서도 삭제 구현해야 함.
    - 굉장히 짜파게티 코드라 렌더링 시간 등 cost가 심한데 이를 어떻게 할지 논의
    - 정산탭에 나오는 계좌정보의 경우 firebase console 에 accountBank, accountNumber field 따로 제작, 추후 세팅탭에서 관리해주어야 함.

--- src/screens/CalculationScreen.js
+++ src/screens/CalculationScreen.js
@@ -12,7 +12,7 @@ export default class CalculationScreen extends Component {
         super(props)
         this.state = {
             calculationList: [],
-            hostList: [],
+            hostList: [], 
             sendList: []
         }
 
