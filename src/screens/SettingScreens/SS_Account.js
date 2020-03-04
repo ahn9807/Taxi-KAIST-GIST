9de1780729534b311f41859a6c579b9d2366ebb4
안준호 <junho@anjunhoui-iMac.local>
2020-03-04 19:05:20 +0900

    3/4 junho logout 메소드 추가

--- src/screens/SettingScreens/SS_Account.js
+++ src/screens/SettingScreens/SS_Account.js
@@ -3,6 +3,18 @@ import { ImageBackground, StyleSheet, View } from "react-native";
 import { Icon, Button, Header, Text, Divider } from "react-native-elements";
 
 export default class SS_Account extends Component {
+    onLogout = () => {
+        firebase.auth().signOut()
+        AsyncStorage.setItem("@loggedInUserID:uid", '')
+        AsyncStorage.setItem("@loggedInUserID:email", '')
+        AsyncStorage.setItem("@loggedInUserID:password", '')
+        this.props.navigation.navigate('Welcome')
+    }
+
+    onChnageID = () => {
+
+    }
+    
     render() {
         return(
             <View>
